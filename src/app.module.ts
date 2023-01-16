67757c235c7563fb7651aecadee23c5a62e892dd
Hemang Rathod <hemang.rathod@brainvire.com>
2023-01-16 19:18:10 +0530

    Made book crud api using nest and mongodb

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BookModule } from './book/book.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [".local.env"]
+    }),
+    MongooseModule.forRootAsync({
+      imports : [ConfigModule],
+      useFactory: (configService: ConfigService)=> ({uri: configService.get("MONGO_URI")}),
+      inject: [ConfigService]
+    }),
+    BookModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
